21466eadc0204cfb68d6f9456b1ac5aebc79c10c
sagar <sagar@engineerbabu.com>
2020-03-20 16:12:50 +0530

    submit issue

---
+++ IncidentReport/IncidentReport/ViewController/SubmitIssueViewController/SubmitIssueViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SubmitIssueViewController.swift
+//  IncidentReport
+//
+//  Created by Apple on 20/03/20.
+//  Copyright © 2020 Arpita Jain. All rights reserved.
+//
+
+import UIKit
+
+class SubmitIssueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
